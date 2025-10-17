68e8590844d2e74edc9b38e70335eba6553c5d08
Shahid Khan <shahidseran786@gmail.com>
2025-10-17 12:52:49 +0530

    dynamic URL added

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
+const currentScript = document.currentScript;
+const url = new URL(currentScript.src);
+const param = url.searchParams.get("param");
+
+if (param) {
+    console.log("Param from script URL:", param);
+    // you can call a function here
+}
+
 const greetFn=(name)=>console.log(`Hello ${name} !`)
+greetFn(param)
 let dummy="dummy"
